ff46f08cf4cdf0a7148607a55893217e3c09bc9a
LuisPassosRamos <20222tadssaj0015@ifba.edu.br>
2025-03-25 18:54:56 -0300

    refactor: improve brand table population insert

--- etapa1/inserts/q3_brandsInserts.sql
+++ etapa1/inserts/q3_brandsInserts.sql
@@ -1,11 +1,3 @@
-INSERT INTO public.brands (name) VALUES
-('Apple'), ('Samsung'), ('Sony'), ('LG'), ('Dell'), ('HP'), ('Lenovo'), ('Asus'), ('Acer'), ('Microsoft'),
-('Nike'), ('Adidas'), ('Puma'), ('Reebok'), ('Under Armour'), ('Fila'), ('New Balance'), ('Mizuno'), ('Oakley'), ('Speedo'),
-('Philips'), ('Electrolux'), ('Brastemp'), ('Consul'), ('Mondial'), ('Arno'), ('Britânia'), ('Fischer'), ('Cadence'), ('KitchenAid'),
-('Nestlé'), ('Coca-Cola'), ('Pepsi'), ('Heinz'), ('Danone'), ('Bauducco'), ('Yoki'), ('Sadia'), ('Perdigão'), ('JBS'),
-('L''Oreal'), ('Natura'), ('Avon'), ('O Boticário'), ('Vichy'), ('Neutrogena'), ('La Roche-Posay'), ('Maybelline'), ('MAC'), ('Revlon'),
-('Casas Bahia'), ('Magazine Luiza'), ('Amazon'), ('Mercado Livre'), ('Americanas'), ('AliExpress'), ('Shopee'), ('eBay'), ('Submarino'), ('Fast Shop'),
-('GoPro'), ('Canon'), ('Nikon'), ('Fujifilm'), ('Panasonic'), ('Bose'), ('JBL'), ('Beats by Dre'), ('Harman Kardon'), ('Logitech'),
-('Bosch'), ('Makita'), ('DeWalt'), ('Stanley'), ('Black & Decker'), ('3M'), ('Caterpillar'), ('Vonder'), ('Tramontina'), ('Skil'),
-('Toyota'), ('Ford'), ('Chevrolet'), ('Volkswagen'), ('Honda'), ('Fiat'), ('Jeep'), ('BMW'), ('Mercedes-Benz'), ('Hyundai'),
-('Intel'), ('AMD'), ('NVIDIA'), ('Qualcomm'), ('Cisco'), ('Oracle'), ('SAP'), ('IBM'), ('Red Hat'), ('VMware');
+INSERT INTO public.brands (name)
+SELECT 'Marca ' || i
+FROM generate_series(1, 500) AS i;
