05415de7b99e3b9597c23d1442e2f4cb8749c5f1
kaiostavares <kaiosandek@outlook.com>
2025-04-03 16:35:34 -0300

    feat: update queries

---
+++ etapa1/inserts/q5_paymentMethodsInserts.sql
@@ -0,0 +1,6 @@
+INSERT INTO public.payment_methods(
+	name, description)
+	VALUES ('CREDITO', 'Compras feitas no cartão de crédito'),
+	('DEBITO', 'Compras feitas no cartão de débito'),
+	('PIX', 'Compras pelo pix'),
+	('BOLETO', 'Compras feitas no boleto');
--- etapa1/inserts/q7_cartsInsert.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO public.carts (user_id)
-SELECT i FROM generate_series(1, 10001) AS i;
